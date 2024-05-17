2daf2d5c20857c3988cb80d709b1007e092a7686
kartiksharma76 <kartiksharma768976@gmail.com>
2024-05-17 22:24:16 +0530

    Collage.added

---
+++ Java17/src/javabasics/Collage.java
@@ -0,0 +1,74 @@
+package javabasics;
+
+public class Collage {
+	public static String department="mechenical,civil,cs,mca,it";
+	public static int student = 4000;
+	public static int  UG  = 3000;
+	public static int PG = 1000;
+	public static int faculty = 100;
+	public static int  teachingstaff  =80;
+	public static int  nonteachingstaff =10;
+	public static int Libraryin = 10;
+	public static int computerlab = 10;
+	public static int totalclasses = 200;
+	public static int fees = 60000;
+	public static int bus =10;
+	public static int hostel = 4;
+	public static String boyshostelname ="jc bose ,cv raman ";
+	public static String girlshostelname = "mother teresa ,kalpana chawala";
+	public static int park = 2;
+	public static int collagecode= 0133;
+	public static String collagename = "SIRT";
+	
+ public static void main(String []args) {
+	new Collage();
+	String department= Collage.department;
+	System.out.println(department);
+// system.out.println(college.department);
+int student = Collage.student ;
+System.out.println(student);
+int UG = Collage.UG ;
+System.out.println(UG);
+int PG = Collage.PG ;
+System.out.println(PG);
+int faculty =Collage.faculty;
+System.out.println(faculty);
+int teachingstaff = Collage.teachingstaff;
+System.out.println(teachingstaff);
+int nonteachingstaff =Collage.nonteachingstaff;
+System.out.println(nonteachingstaff);
+int libraryin =Collage.Libraryin;
+System.out.println(libraryin);
+int computerlab =Collage.computerlab;
+System.out.println(computerlab);
+int totalclasses =Collage.totalclasses;
+System.out.println(totalclasses);
+int fees =Collage.fees;
+System.out.println(fees);
+int bus = Collage.bus;
+System.out.println(bus);
+int hostel = Collage.hostel;
+System.out.println(hostel);
+String boyshostelname =Collage.boyshostelname;
+System.out.println(boyshostelname);
+String girlshostelname =Collage.girlshostelname;
+System.out.println(girlshostelname);
+int park = Collage.park;
+System.out.println(park);
+int collagecode =Collage.collagecode;
+System.out.println(collagecode);
+String collagename = Collage.collagename;
+System.out.println(collagename);
+
+
+
+
+
+
+
+	
+ }
+	
+	
+
+}
