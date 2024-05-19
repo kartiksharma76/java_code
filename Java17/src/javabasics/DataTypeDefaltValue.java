6e6492dde0a6bc59dd7b1fe3c2e49290054e1c4c
kartiksharma76 <kartiksharma768976@gmail.com>
2024-05-19 13:34:40 +0530

    DataTypeDefaltValue.added

---
+++ Java17/src/javabasics/DataTypeDefaltValue.java
@@ -0,0 +1,25 @@
+package javabasics;
+
+public class DataTypeDefaltValue {
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static float f;
+	static double d;
+	static char c;
+	static boolean bol;
+	static String str;
+
+	public static void main(String[] args) {
+		System.out.println(DataTypeDefaltValue.b);
+		System.out.println(DataTypeDefaltValue.s);
+		System.out.println(DataTypeDefaltValue.i);
+		System.out.println(DataTypeDefaltValue.l);
+		System.out.println(DataTypeDefaltValue.f);
+		System.out.println(DataTypeDefaltValue.d);
+		System.out.println(DataTypeDefaltValue.bol);
+		System.out.println(DataTypeDefaltValue.str);
+
+	}
+}
