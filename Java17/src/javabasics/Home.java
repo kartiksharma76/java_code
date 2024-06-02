d50d98d992ba31427d546459ca07a20fd3f17d0c
kartiksharma76 <kartiksharma768976@gmail.com>
2024-06-03 01:20:34 +0530

    added

--- Java17/src/javabasics/Home.java
+++ Java17/src/javabasics/Home.java
@@ -1,27 +1,27 @@
 package javabasics;
 
 public class Home {
-public static int member=5;
-public static String fathername ="kamlesh kumar sharma";
-public static String mothername="anarkali sharma";
-public static String brothername="anshu sharma";
-public static String Sistername="preeti sharma";
-public static String myname="kartik sharma";
+	public static int member = 5;
+	public static String fathername = "kamlesh kumar sharma";
+	public static String mothername = "anarkali sharma";
+	public static String brothername = "anshu sharma";
+	public static String Sistername = "preeti sharma";
+	public static String myname = "kartik sharma";
 
-public static void main(String[] args) {
-	int member=Home.member;
-	System.out.println(member);
-	// system.out.println(Home.member);
-	String fathername=Home.fathername;
-	System.out.println(fathername);
-	String mothername=Home.mothername;
-	System.out.println(mothername);
-	String brothername=Home.brothername;
-	System.out.println(brothername);
-	String sistername=Home.Sistername;
-	System.out.println(sistername);
-	String myname=Home.myname;
-	System.out.println(myname);
-	
-}
+	public static void main(String[] args) {
+		int member = Home.member;
+		System.out.println(member);
+		// system.out.println(Home.member);
+		String fathername = Home.fathername;
+		System.out.println(fathername);
+		String mothername = Home.mothername;
+		System.out.println(mothername);
+		String brothername = Home.brothername;
+		System.out.println(brothername);
+		String sistername = Home.Sistername;
+		System.out.println(sistername);
+		String myname = Home.myname;
+		System.out.println(myname);
+
+	}
 }
