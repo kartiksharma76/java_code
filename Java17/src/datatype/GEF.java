78c9ad5f1d1d572d3ef1eafe2725a2624a9dfcb2
kartiksharma76 <kartiksharma768976@gmail.com>
2024-06-07 20:19:19 +0530

    added.datatype

--- Java17/src/datatype/GEF.java
+++ Java17/src/datatype/GEF.java
@@ -2,20 +2,20 @@ package datatype;
 
 public class GEF {
 	public static void main(String[] args) {
-		char a ='G';
-		int i =89;
-		System.out.println(" char"+a);
-		System.out.println("int"+ i);
+		char a = 'G';
+		int i = 89;
+		System.out.println(" char" + a);
+		System.out.println("int" + i);
 		double d = 4.366666635533;
-		float f= 4.4744884f;
-		long l= 32323;
-		System.out.println("double"+d);
-		System.out.println("float"+f);
-		System.out.println("long"+l);
-		short s=56;
-		System.out.println("short"+s);
-		byte b=2;
-		System.out.println("byte"+ b);
+		float f = 4.4744884f;
+		long l = 32323;
+		System.out.println("double" + d);
+		System.out.println("float" + f);
+		System.out.println("long" + l);
+		short s = 56;
+		System.out.println("short" + s);
+		byte b = 2;
+		System.out.println("byte" + b);
 	}
 
 }
