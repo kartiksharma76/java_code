1c6dc135c0cb2d678083f45b61d623b00d5af50e
kartiksharma76 <kartiksharma768976@gmail.com>
2024-06-11 21:49:36 +0530

    operater.added

---
+++ Java17/src/operaters/LeftShift.java
@@ -0,0 +1,23 @@
+package operaters;
+
+public class LeftShift {
+	public static void main(String[] args) {
+		int a = -7;
+		int b = a << 2;
+		System.out.println(b);
+
+		int e = -19;
+		int c = e << 2;
+		System.out.println(c);
+
+		int d = -57;
+		int v = d << 2;
+		System.out.println(v);
+
+		int t = -90;
+		int g = t << 2;
+		System.out.println(t);
+
+	}
+
+}
---
+++ Java17/src/operaters/RightShiftOperaters.java
@@ -0,0 +1,22 @@
+package operaters;
+
+public class RightShiftOperaters {
+	public static void main(String[] args) {
+		int a = -4;
+		int b = a >> 2;
+		System.out.println(b);
+		int c = 37;
+		int d = c >> 2;
+		System.out.println(d);
+		int w = 7;
+		int r = w >> 3;
+		System.out.println(r);
+		int y = 45;
+		int g = y >> 2;
+		System.out.println(g);
+		int j = -67;
+		int x = j >> 2;
+		System.out.println(x);
+	}
+
+}
