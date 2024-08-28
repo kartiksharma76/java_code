2a49d12785a76b95f859f3c9634c92e09de956ef
kartiksharma76 <kartiksharma768976@gmail.com>
2024-08-28 12:16:32 +0530

    .added

--- Java17/src/controlflow/Loop2.java
+++ Java17/src/controlflow/Loop2.java
@@ -2,9 +2,15 @@ package controlflow;
 
 public class Loop2 {
 	public static void main(String[] args) {
-		for(int counter =0;  counter<11; counter++) {
+		for (int counter = 0; counter < 11; counter++) {
 			System.out.print(counter);
 		}
+		for (int i=0; i<11; i++) {
+			System.out.println(i);
+		}
+		for ( int i=0; i<11; i++) {
+		System.out.println("i");	
 	}
 
+}
 }
