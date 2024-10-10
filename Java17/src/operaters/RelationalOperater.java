6647298bda87620be3a44e32504fff0b5b677694
kartiksharma76 <kartiksharma768976@gmail.com>
2024-10-10 15:30:24 +0530

    operater.added

---
+++ Java17/src/operaters/RelationalOperater.java
@@ -0,0 +1,14 @@
+package operaters;
+
+public class RelationalOperater {
+	public static void main(String[] args) {
+		String name = "kartik";
+		String name1 = "aman";
+		System.out.println(name1 == name);
+		if (name1 == name) {
+			return;
+		} else {
+			return;
+		}
+	}
+}
