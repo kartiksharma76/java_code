4ab929b6c4eb3a6f4316b4775be4f8b74a7a3af7
kartiksharma76 <kartiksharma768976@gmail.com>
2024-10-17 00:43:13 +0530

    added

---
+++ Java17/src/controlflow/BasicMaths14.java
@@ -0,0 +1,13 @@
+package controlflow;
+
+public class BasicMaths14 {
+	public static void main(String[] args) {
+		for (int table = 1; table <= 100; table++) {
+			for (int i = 1; i <= 10; i++) {
+				System.out.println(table + "*" + (i) + "=" + (table * (i)));
+			}
+			System.out.println("-----------------------");
+
+		}
+	}
+}
---
+++ Java17/src/controlflow/Table1To10.java
@@ -0,0 +1,67 @@
+package controlflow;
+
+public class Table1To10 {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("table of one:");
+		int a = 1;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(a + "*" + i + "=" + a * i);
+		}
+		System.out.println();
+		System.out.println("table of two:");
+		int b = 2;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(b + "*" + i + "=" + b * i);
+		}
+		System.out.println();
+		System.out.println("table of three:");
+		int c = 3;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(c + "*" + i + "=" + c * i);
+		}
+		System.out.println();
+		System.out.println("table of four:");
+		int d = 4;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(d + "*" + i + "=" + d * i);
+
+		}
+		System.out.println();
+		System.out.println("table of five:");
+		int e = 5;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(e + "*" + i + "=" + d * i);
+		}
+		System.out.println();
+		System.out.println("table of six:");
+		int f = 6;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(f + "*" + i + "=" + f * i);
+		}
+		System.out.println();
+		System.out.println("table of seven:");
+		int g = 7;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(g + "*" + i + "=" + g * i);
+		}
+		System.out.println();
+		System.out.println("table of eight:");
+		int h = 8;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(h + "*" + i + "=" + h * i);
+		}
+		System.out.println();
+		System.out.println("table of nine:");
+		int j = 9;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(j + "*" + i + "=" + j * i);
+		}
+		System.out.println();
+		System.out.println("table of ten:");
+		int k = 10;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(k + "*" + i + "=" + k * i);
+		}
+	}
+}
