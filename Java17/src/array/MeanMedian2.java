f15882febf04db5b016d09e7c84dbdaf1f460f34
kartiksharma76 <kartiksharma768976@gmail.com>
2024-10-29 22:06:30 +0530

    array.added

---
+++ Java17/src/array/MeanMedian2.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Arrays;
+
+public class MeanMedian2 {
+	public static void main(String[] args) {
+		int[] arr = { 12, 23, 34, 45, 15 };
+		double mean = calculateMean(arr);
+		double median = calculateMedian(arr);
+
+		System.out.println("mean:" + mean);
+		System.out.println("median:" + median);
+	}
+
+	private static double calculateMedian(int[] arr) {
+		int sum = 0;
+		for (int num : arr) {
+			sum += num;
+
+		}
+		return (double) sum / arr.length;
+
+	}
+
+	private static double calculateMean(int[] arr) {
+		Arrays.sort(arr);
+		int n = arr.length;
+		if (n % 2 == 0) {
+			int mid1 = arr[n / 2 - 1];
+			int mid2 = arr[n / 2];
+
+			return (double) (mid1 + mid2) / 2;
+		} else {
+			return arr[n / 2];
+		}
+	}
+}
