79d3aba5758c75a1004c1e73188f1a8e3fb6adfe
kartiksharma76 <kartiksharma768976@gmail.com>
2024-11-27 20:54:06 +0530

    tree.added

---
+++ Java17/src/tree/AvlTreeTest2.java
@@ -0,0 +1,16 @@
+package tree;
+
+public class AvlTreeTest2 {
+	public static void main(String[] args) {
+		AvlTree2 avl = new AvlTree2();
+		avl.insert(10);
+		avl.insert(20);
+		avl.insert(30);
+		avl.insert(40);
+		avl.insert(50);
+		avl.insert(60);
+		avl.insert(70);
+		avl.insert(90);
+		avl.preOrder();
+	}
+}
