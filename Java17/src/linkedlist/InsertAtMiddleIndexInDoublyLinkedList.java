03a87e557825b93b1118f605ac15968dbe30a2e5
kartiksharma76 <kartiksharma768976@gmail.com>
2024-12-27 02:21:22 +0530

    linkedlist.added

---
+++ Java17/src/linkedlist/InsertAtMiddleIndexInDoublyLinkedList.java
@@ -0,0 +1,62 @@
+package linkedlist;
+
+public class InsertAtMiddleIndexInDoublyLinkedList {
+	private Node head;
+	private Node tail;
+	private int size;
+
+	public void insert(int data) {
+		Node node = new Node(data, null, null);
+		if (head == null) {
+			head = node;
+			tail = head;
+			size++;
+		} else {
+			tail.next = node;
+			node.previous = tail;
+			tail = node;
+			size++;
+		}
+	}
+
+	public void insertAtMid(int position, int data) {
+		Node node = new Node(data, null, null);
+		Node temp = head;
+		for (int i = 1; i < position; i++) {
+			temp = temp.next;
+
+		}
+		node.next = temp;
+		node.previous = temp.previous;
+		temp.previous.next = node;
+		temp.previous = node;
+		size++;
+
+	}
+
+	public void print() {
+		Node temp = head;
+		for (int i = 0; i < size; i++) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+	}
+
+	public int size() {
+		return size;
+
+	}
+
+	class Node {
+		private int data;
+		private Node next;
+		private Node previous;
+
+		public Node(int data, Node next, Node previous) {
+			super();
+			this.data = data;
+			this.next = next;
+			this.previous = previous;
+		}
+	}
+}
