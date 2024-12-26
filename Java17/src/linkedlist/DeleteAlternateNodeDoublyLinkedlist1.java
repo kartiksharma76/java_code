b99c8a11b38135c6baaf3eb3dabd616610554322
kartiksharma76 <kartiksharma768976@gmail.com>
2024-12-27 02:20:55 +0530

    linkedlist.added

---
+++ Java17/src/linkedlist/DeleteAlternateNodeDoublyLinkedlist1.java
@@ -0,0 +1,63 @@
+package linkedlist;
+
+import linkedlist.DeleteAlternateNodeDoublyLinkedlist.Node;
+
+public class DeleteAlternateNodeDoublyLinkedlist1 {
+	private Node head;
+	private Node tail;
+	private int size;
+
+	public void insert(int data) {
+		Node node = new Node(data, null, null);
+		if (head == null) {
+			head = node;
+			tail = head;
+			size++;
+		} else {
+			tail.next = node;
+			node.previous = tail;
+			tail = node;
+			size++;
+		}
+	}
+
+	public void deleteAlternateNodes() {
+		if (head == null) {
+			return;
+		}
+		Node temp = head;
+		while (temp != null && temp.next != null) {
+			temp.next = temp.next.next;
+
+			temp = temp.next;
+		}
+
+	}
+
+	public void print() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+
+	}
+
+	public int size() {
+		return size;
+
+	}
+
+	class Node {
+		private int data;
+		private Node next;
+		private Node previous;
+
+		public Node(int data, Node next, Node previous) {
+			super();
+			this.data = data;
+			this.next = next;
+			this.previous = previous;
+		}
+	}
+}
