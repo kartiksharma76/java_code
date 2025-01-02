faaad8c7534c07694e68a5e0bc73d600826b5827
kartiksharma76 <kartiksharma768976@gmail.com>
2025-01-03 00:19:55 +0530

    inheritance.added

---
+++ Java17/src/Inheritance/CheakingAccount.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public interface CheakingAccount {
+	public void deposit(double amount);
+
+	public void withdraw(double amount);
+
+	public double getBalance();
+
+}
---
+++ Java17/src/Inheritance/SavingAccount.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public interface SavingAccount {
+	public void deposit(double amount);
+
+	public void withdraw(double amount);
+
+	public double getInterestRate();
+}
