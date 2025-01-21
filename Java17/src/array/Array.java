642be3844ca7e1a55c4fb7c9907a3819309df1de
kartiksharma76 <kartiksharma768976@gmail.com>
2025-01-21 19:18:21 +0530

    array.added

---
+++ Java17/src/array/Array.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Array {
+public static void main(String[] args) {
+	 int []arr = {80,90,85};
+	 for(int i=0; i<arr.length;i++);
+	System.out.println(arr);
+	System.out.println(arr[0]);
+	
+}
+
+}
---
+++ Java17/src/array/Array2.java
@@ -0,0 +1,11 @@
+package array;
+
+import java.util.Arrays;
+
+public class Array2 {
+
+public static void main(String[] args) {
+	int []array= {1,2,3,4,5};
+//	int sum=Arrays.stream(array).filter( n%2==0);
+}
+}
