4c9e453f1d12f9cc450a12519dfe8f16508ad150
kartiksharma76 <kartiksharma768976@gmail.com>
2025-01-22 11:38:53 +0530

    operators.added

---
+++ Java17/src/operaters/Animal.java
@@ -0,0 +1,13 @@
+package operaters;
+
+public class Animal {
+public void eat() { 
+	System.out.println("Animal eat");
+}
+}
+class Dog extends Animal{
+	public void runner() {
+		System.out.println("dog runner");
+	}
+}
+	
