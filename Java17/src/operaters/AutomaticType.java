e9a7e25b071a850d28f10556d83811254c3d7b20
kartiksharma76 <kartiksharma768976@gmail.com>
2025-01-22 11:40:03 +0530

    operaters.added

---
+++ Java17/src/operaters/AutomaticType.java
@@ -0,0 +1,12 @@
+package operaters;
+
+public class AutomaticType {
+	public static void main(String[] args) {
+		int i = 100;
+		long l = i;
+		double d = l;
+		System.out.println("int:" + i);
+		System.out.println("long:" + l);
+		System.out.println("double:" + d);
+	}
+}
