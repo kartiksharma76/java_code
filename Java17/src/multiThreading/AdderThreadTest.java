33ee1108154b074aad6750685f187763f755d8ce
kartiksharma76 <kartiksharma768976@gmail.com>
2025-03-30 00:32:51 +0530

    multiThreading.added

---
+++ Java17/src/multiThreading/AdderThreadTest.java
@@ -0,0 +1,8 @@
+package multiThreading;
+
+public class AdderThreadTest {
+public static void main(String[] args) {
+	AdderThread adderthread=new AdderThread();
+	Thread tread=new Thread();
+}
+}
---
+++ Java17/src/multiThreading/ThreadPrevention.java
@@ -0,0 +1,16 @@
+package multiThreading;
+// yield join sleep
+public class ThreadPrevention {
+public static void main(String[] args) {
+//	Thread thread=new Thread();
+//	System.out.println(thread.getPriority());
+//	thread.setPriority(10);// we can set the property in own need like 1 to 10
+//	System.out.println(thread.getPriority());// range of property is 1 to 10
+//	System.out.println(thread.getName());//thread 0
+//	System.out.println("my thread");
+//	System.out.println(Thread.currentThread().getName());
+
+	
+	
+}
+}
