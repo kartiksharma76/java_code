d361a8cf80a78f01eddf2c5c9343ab88d4a3576c
kartiksharma76 <kartiksharma768976@gmail.com>
2025-04-23 23:11:15 +0530

    ArrayQuestion.added

---
+++ Java17/src/ArrayQuestion/Palindrome1.java
@@ -0,0 +1,19 @@
+package ArrayQuestion;
+
+public class Palindrome1 {
+	public static boolean isPalindrome(int[] arr) {
+		int n = arr.length;
+		for (int i = 0; i < n / 2; i++) {
+			if (arr[i] != arr[n - 1 - i]) {
+				return false;
+			}
+		}
+		return true;
+
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+		System.out.println(isPalindrome(arr));
+	}
+}
