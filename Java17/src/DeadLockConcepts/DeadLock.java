f3f209d781f47c151c806d2bc8641fa3df29243e
kartiksharma76 <kartiksharm768976@gmail.com>
2025-06-17 19:06:22 +0530

    deadlock.added

---
+++ Java17/src/DeadLockConcepts/DeadLock.java
@@ -0,0 +1,31 @@
+package DeadLockConcepts;
+
+public class DeadLock {
+	public void methodA() {
+		synchronized (String.class) {
+			System.out.println(Thread.currentThread().getName() + "String.class");
+			synchronized (Integer.class) {
+				System.out.println(Thread.currentThread().getName() + "lock Integer.class");
+
+			}
+		}
+	}
+
+	public void methodB() {
+		synchronized (String.class) {
+			System.out.println(Thread.currentThread().getName() + "lock Integer.class");
+			synchronized (String.class) {
+				System.out.println(Thread.currentThread().getName() + "String.class");
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		DeadLock lock = new DeadLock();
+		Thread t1 = new Thread(() -> lock.methodA(), "thread-1");
+		Thread t2 = new Thread(() -> lock.methodB(), "thread-1");
+		t1.start();
+		t2.start();
+	}
+}
