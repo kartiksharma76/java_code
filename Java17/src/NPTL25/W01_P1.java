bfe14b99794ecc9a10877a9ad1f48284a79905d0
kartiksharma76 <kartiksharm768976@gmail.com>
2025-08-25 19:00:22 +0530

    NPTL.added

---
+++ Java17/src/NPTL25/W01_P1.java
@@ -0,0 +1,17 @@
+package NPTL25;
+
+import java.util.Scanner;
+
+public class W01_P1 {
+public static void main(String[] args) {
+	Scanner sc= new Scanner(System.in);
+	int number = sc.nextInt();
+	if(number>=0) {
+		System.out.println("positive");
+		
+	}else {
+		System.out.println("negative");
+	}
+	sc.close();
+}
+}
