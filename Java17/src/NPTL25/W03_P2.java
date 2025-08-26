095c5c36dc6058a8ec179dbae9cb30d9b95a3384
kartiksharma76 <kartiksharm768976@gmail.com>
2025-08-26 08:42:17 +0530

    NPTL25.added

---
+++ Java17/src/NPTL25/W03_P2.java
@@ -0,0 +1,15 @@
+package NPTL25;
+
+import java.util.Scanner;
+
+public class W03_P2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				System.out.println(i + " ");
+			}
+		}
+	}
+}
