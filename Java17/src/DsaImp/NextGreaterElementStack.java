d96567f974d6c71649a10beab779a77620a8a138
kartik <kartiksharma768976@gmail.com>
2025-09-22 01:14:20 +0530

    Dsa.added

---
+++ Java17/src/DsaImp/NextGreaterElementStack.java
@@ -0,0 +1,36 @@
+package DsaImp;
+
+import java.util.Stack;
+
+public class NextGreaterElementStack {
+	public static int[] nextGreaterElement(int[] arr) {
+		int n = arr.length;
+		int[] result = new int[n];
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i = n - 1; i >= 0; i--) {
+			while (!stack.isEmpty() && arr[i] >= arr[stack.peek()]) {
+				stack.pop();
+			}
+
+			if (stack.isEmpty()) {
+				result[i] = -1;
+			} else {
+				result[i] = arr[stack.peek()];
+			}
+
+			stack.push(i);
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 4, 2, 1, 5, 3 };
+		int[] result = nextGreaterElement(arr);
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + " ");
+		}
+	}
+}
