86c700e177a473bc7398cd2b137003b204208caa
kartik <kartiksharma768976@gmail.com>
2025-10-09 11:53:13 +0530

    t&p.added

---
+++ Java17/src/TraningAdPlacement/ComparisonOperators.java
@@ -0,0 +1,15 @@
+package TraningAdPlacement;
+
+public class ComparisonOperators {
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 200;
+		System.out.println(a > b);
+		System.out.println(a >= b);
+		System.out.println(a == b);
+		System.out.println(a != b);
+		System.out.println(a <= b);
+		System.out.println(a < b);
+
+	}
+}
