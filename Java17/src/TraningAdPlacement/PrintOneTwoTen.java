a2c9095d3ecd3adcb6ee123d8f4b9affa9db780f
kartik <kartiksharma768976@gmail.com>
2025-10-28 00:16:03 +0530

    t&p.added

---
+++ Java17/src/TraningAdPlacement/PrintOneTwoTen.java
@@ -0,0 +1,10 @@
+package TraningAdPlacement;
+
+public class PrintOneTwoTen {
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 10) {
+			System.out.println(i++ + " ");
+		}
+	}
+}
