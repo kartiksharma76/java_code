2b14c1b1945ff94f1a7b0c985837a570a52544ec
kartik <kartiksharma768976@gmail.com>
2025-12-11 22:12:44 +0530

    .added

---
+++ Java17/src/telusco/ComparisionOperators.java
@@ -0,0 +1,12 @@
+package telusco;
+
+public class ComparisionOperators {
+	public static void main(String[] args) {
+		
+	
+double x = 8.8;
+double y =9.8;
+boolean result = x >=y;
+System.out.println(result);
+}
+}
