df5fa72c5fa059fa2a692393cfac774630e8cc89
kartik <kartiksharma768976@gmail.com>
2025-12-12 02:00:00 +0530

    .added

---
+++ Java17/src/telusco/DataTypes.java
@@ -0,0 +1,17 @@
+package telusco;
+
+public class DataTypes {
+public static void main(String[] args) {
+//	int num1 =9;
+//	byte by = 127;
+//	short sh = 5558;
+//	long l= 5854l;
+//	float f = 5.8f;
+//	double d = 5.8;
+//	char c ='k';
+//	boolean b =true;
+	int num1 = 0x7E;
+	System.out.println(num1);
+	
+}
+}
