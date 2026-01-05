ad9dfd84dc687c4b698d43670fc88c56e14331da
kartik <kartiksharma768976@gmail.com>
2026-01-06 01:32:52 +0530

    .added

---
+++ Java17/src/Practicesheet/ElseIf.java
@@ -0,0 +1,13 @@
+package Practicesheet;
+
+public class ElseIf {
+public static void main(String[] args) {
+	int a = 10;
+	int b = 20;
+	if(a>b) {
+		System.out.println("A is greater");
+	}else {
+		System.out.println("B is greater");
+	}
+}
+}
