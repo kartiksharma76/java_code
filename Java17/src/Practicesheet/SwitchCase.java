bda39a8f2dd4a325c286a1311ac26e1b22a3d815
kartik <kartiksharma768976@gmail.com>
2026-01-27 20:26:46 +0530

    .added

---
+++ Java17/src/Practicesheet/SwitchCase.java
@@ -0,0 +1,17 @@
+package Practicesheet;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		int n = 2;
+		switch (n) {
+		case 1:
+			System.out.println("One");
+			break;
+		case 2:
+			System.out.println("Two");
+			break;
+		default:
+			System.out.println("Invalid");
+		}
+	}
+}
