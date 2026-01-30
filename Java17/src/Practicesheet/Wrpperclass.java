2caefbaac1dbcdea866debcf26c3225fb13ffafe
kartik <kartiksharma768976@gmail.com>
2026-01-30 22:21:28 +0530

    .added

---
+++ Java17/src/Practicesheet/Wrpperclass.java
@@ -0,0 +1,21 @@
+package Practicesheet;
+
+public class Wrpperclass {
+	public static void main(String[] args) {
+		Integer a = 100;
+		Integer b = 100;
+		Integer c = 100;
+		Integer d = 100;
+		System.out.println(a == b);
+		System.out.println(c == d);
+
+//		Integer intObj = Integer.valueOf(100);
+//		Double doubleObj = Double.valueOf(100.0);
+//		int primitiveInt = intObj;
+//		double primitiveIntDouble = doubleObj;
+//		Integer newIntObj = primitiveInt;
+//		Double newDoubleObj = primitiveIntDouble;
+//
+//		System.out.println(intObj.equals(newIntObj) + " " + doubleObj.equals(newDoubleObj));
+	}
+}
