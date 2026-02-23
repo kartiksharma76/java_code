4a9b9b835d21f408c35cb672ca4e01c17f792cbe
kartik <kartiksharma768976@gmail.com>
2026-02-24 02:09:56 +0530

    collection.added

---
+++ Java17/src/Collection2026/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package Collection2026;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>();
+
+		list.add("Java Devloper:");
+		list.add("Python programmer:");
+		list.add("C++ programmer");
+
+		System.out.println(list);
+	}
+}
