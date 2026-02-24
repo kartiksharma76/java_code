20f14916704e6fd214b85fe425d6505f00ae4fba
kartik <kartiksharma768976@gmail.com>
2026-02-25 03:44:23 +0530

    collection.added

---
+++ Java17/src/Collection2026/HashSetDemo.java
@@ -0,0 +1,15 @@
+package Collection2026;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+	 public static void main(String[] args) {
+	        HashSet<String> set = new HashSet<>();
+
+	        set.add("kartik");
+	        set.add("kirtan");
+	        set.add("twinkle"); // duplicate not allowed
+
+	        System.out.println(set);
+	    }
+}
---
+++ Java17/src/Collection2026/LinkdeHashSetDemo.java
@@ -0,0 +1,15 @@
+package Collection2026;
+
+import java.util.LinkedHashSet;
+
+public class LinkdeHashSetDemo {
+	public static void main(String[] args) {
+		LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+		set.add(3);
+		set.add(1);
+		set.add(2);
+
+		System.out.println(set);
+	}
+}
