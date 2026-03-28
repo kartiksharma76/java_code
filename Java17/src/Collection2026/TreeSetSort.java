7700a1a0f5c974d563abf3ba5b532a4811b35073
kartik <kartiksharma768976@gmail.com>
2026-03-28 22:34:04 +0530

    collction.added

---
+++ Java17/src/Collection2026/TreeSetSort.java
@@ -0,0 +1,14 @@
+package Collection2026;
+
+import java.util.TreeSet;
+
+public class TreeSetSort {
+	public static void main(String[] args) {
+		TreeSet<Integer> set = new TreeSet<>();
+		set.add(50);
+		set.add(10);
+		set.add(30);
+
+		System.out.println(set);
+	}
+}
