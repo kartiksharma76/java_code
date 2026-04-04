4f5d8258503cc48ced2bac1de3ce00671040a1b2
kartik <kartiksharma768976@gmail.com>
2026-04-04 13:41:16 +0530

    java.added

--- Java17/src/Stack/DeleteMidTest.java
+++ Java17/src/Stack/DeleteMidTest.java
@@ -1,13 +1,13 @@
-package Stack;
-
-public class DeleteMidTest {
-	public static void main(String[] args) {
-		DeleteMid stack = new DeleteMid();
-		stack.push(10);
-		stack.push(20);
-		stack.push(30);
-		stack.push(40);
-		stack.deletebyposition(2);
-		stack.print();
-	}
-}
+//package Stack;
+//
+//public class DeleteMidTest {
+//	public static void main(String[] args) {
+//		DeleteMid stack = new DeleteMid();
+//		stack.push(10);
+//		stack.push(20);
+//		stack.push(30);
+//		stack.push(40);
+//		stack.deletebyposition(2);
+//		stack.print();
+//	}
+//}
