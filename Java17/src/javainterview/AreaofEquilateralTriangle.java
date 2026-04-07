1119a7a6727eaa71421d1878ad8a02ac27f0965e
kartik <kartiksharma768976@gmail.com>
2026-04-07 20:19:17 +0530

    java.added

---
+++ Java17/src/javainterview/AreaofEquilateralTriangle.java
@@ -0,0 +1,9 @@
+package javainterview;
+
+public class AreaofEquilateralTriangle {
+	public static void main(String[] args) {
+		double a = 5;
+		double area = (Math.sqrt(3) / 4) * a * a;
+		System.out.println("Area = " + area);
+	}
+}
