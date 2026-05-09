c76e80eee1de1b61ee9f9c0adc48012261cdef4d
kartik <kartiksharma768976@gmail.com>
2026-05-10 00:11:14 +0530

    java.added

---
+++ Java17/src/Javaclasses/JavaClassMethods.java
@@ -0,0 +1,18 @@
+package Javaclasses;
+
+public class JavaClassMethods {
+	/**
+	 * Create a method named myMethod() in Main myMethod() print a text (the action
+	 * ) when it is called. To call a method , write the method's name followed by
+	 * two parantheses() and a semicolon
+	 * 
+	 *
+	 */
+	static void MyMethod() {
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) {
+		MyMethod();
+	}
+}
