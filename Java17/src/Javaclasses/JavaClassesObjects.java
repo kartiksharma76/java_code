ae784eff7c9b162d48a485d995df1d86395d4e07
kartik <kartiksharma768976@gmail.com>
2026-05-11 00:12:50 +0530

    java.added

---
+++ Java17/src/Javaclasses/JavaClassesObjects.java
@@ -0,0 +1,20 @@
+package Javaclasses;
+
+public class JavaClassesObjects {
+	/**
+	 * java is a object oriented programming language Every thing is an java
+	 * associated with classes and object along with attribute and method
+	 * 
+	 * 
+	 */
+
+	int x = 5;
+
+	public static void main(String[] args) {
+		JavaClassesObjects myobj = new JavaClassesObjects();
+		JavaClassesObjects myobj1 = new JavaClassesObjects();
+
+		System.out.println(myobj.x);
+		System.out.println(myobj1.x);
+	}
+}
