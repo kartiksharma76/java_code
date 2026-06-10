87cf2f8cc3da2b89ff991302eeb9cd5f12c5b07e
kartik <kartiksharma768976@gmail.com>
2026-06-11 00:40:01 +0530

    java.added

---
+++ Java17/src/array/ReverseArrays.java
@@ -0,0 +1,16 @@
+package array;
+
+public class ReverseArrays {
+	public static void main(String[] args) {
+//		int fact = 1;
+//
+//		for (int i = 1; i <= 5; i++) {
+//			fact *= i;
+//		}
+//		System.out.println(fact);
+
+		for (int i = 10; i >= 1; --i) {
+			System.out.println(i);
+		}
+	}
+}
