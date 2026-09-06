fbdb97bf4df5a8274b38be770c77702d4b8f1816
kartik <kartiksharma768976@gmail.com>
2026-09-07 00:06:18 +0530

    leetcode.added

---
+++ Java17/src/litcode/UniquePathsII.java
@@ -0,0 +1,40 @@
+package litcode;
+
+class UniquePathsII {
+
+	public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+
+		int[] dp = new int[n];
+
+		dp[0] = 1;
+
+		for (int i = 0; i < m; i++) {
+
+			for (int j = 0; j < n; j++) {
+
+				if (obstacleGrid[i][j] == 1) {
+
+					dp[j] = 0;
+
+				} else if (j > 0) {
+
+					dp[j] = dp[j] + dp[j - 1];
+				}
+			}
+		}
+
+		return dp[n - 1];
+	}
+
+	public static void main(String[] args) {
+
+		int[][] obstacleGrid = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+
+		int result = uniquePathsWithObstacles(obstacleGrid);
+
+		System.out.println("Unique Paths = " + result);
+	}
+}
